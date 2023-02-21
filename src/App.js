ed6f3e94dcb2873cc82a6ef118c202428972d34f
Tatiana Chernova <fedoseeva.tv@gmail.com>
2023-02-21 13:28:41 +0200

    First stage of React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/footer";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <Header />
+       
+        <main className="section">
+            <div className="container">
+
+                    <ul className="content-list">
+                        <li className="content-list__item">
+                            <h2 className="title-2">Frontend</h2>
+                            <p>JavaScript, ReactJS, HTML, CSS</p>
+                        </li>
+                        <li className="content-list__item">
+                            <h2 className="title-2">Others</h2>
+                            <p>Git, GitHub, Figma</p>
+                        </li>
+                    </ul>
+            </div>
+        </main>
+
+       <Footer />
     </div>
   );
 }
